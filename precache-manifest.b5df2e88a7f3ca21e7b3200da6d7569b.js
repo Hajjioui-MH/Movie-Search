c4e08543e8dfaed7ae21505ae257c83f45dd3ac2
Hajjioui-MH <strongteethpro@gmail.com>
2020-06-18 17:20:20 +0100

    Updates

---
+++ precache-manifest.b5df2e88a7f3ca21e7b3200da6d7569b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "066fc3ec4c85082d4dcd13b4f73d5544",
+    "url": "/Movie-Search/index.html"
+  },
+  {
+    "revision": "4d007a2e9b8f9ee4e1d2",
+    "url": "/Movie-Search/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e7902c04de97ebc4a153",
+    "url": "/Movie-Search/static/js/2.ca4335bf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Movie-Search/static/js/2.ca4335bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d007a2e9b8f9ee4e1d2",
+    "url": "/Movie-Search/static/js/main.2d5b3ea9.chunk.js"
+  },
+  {
+    "revision": "654f0821e1d6f29506b8",
+    "url": "/Movie-Search/static/js/runtime-main.ec789e46.js"
+  }
+]);

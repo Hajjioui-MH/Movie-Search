a742f5105773f94b06a6a85f0fa04ed3acceb5ff
Hajjioui-MH <hajjioui.mh@gmail.com>
2020-07-11 17:33:55 +0100

    delete ul element

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,11 +5,6 @@ const Navbar = ()=>{
 		<nav>
 		    <div className="nav-wrapper container">
 		      <a href="#" className="logo">Movie Finder</a>
-		      <ul id="nav-mobile" className="right hide-on-med-and-down">
-		        <li><a href="sass.html">Sass</a></li>
-		        <li><a href="badges.html">Components</a></li>
-		        <li><a href="collapsible.html">JavaScript</a></li>
-		      </ul>
 		    </div>
   		</nav>
 	)
